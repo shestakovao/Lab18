d36ce9bc62980e715b19e3b649da866e7e846c1a
Алексей Шестаков <shestakov_ao@bk.ru>
2022-11-07 17:14:02 +0700

    Добавьте файлы проекта.

---
+++ Lab18/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            if (Check(str))
+            {
+                Console.WriteLine("Строка корректна");
+            }
+            else
+            {
+                Console.WriteLine("Строка не корректна");
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool Check(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> sk = new Dictionary<char, char>
+            {
+                {'(', ')' },
+                {'[', ']' },
+                {'{', '}' },
+            };
+            foreach (char c in str)
+            { 
+                if (c == '(' ||  c =='[' ||  c == '{'  ) stack.Push(sk[c]);
+                if (c == ')' || c == ']' || c == '}')
+                {
+                    if (stack.Count == 0 || stack.Pop()!=c)
+                    {
+                        return false;
+                    }                
+                }
+            }
+            if (stack.Count == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
